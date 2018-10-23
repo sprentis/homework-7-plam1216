d6fa7a6d76507025b95dcc5d7b8899f77407d919
Peter <pl1316@nyu.edu>
2018-10-23 00:51:06 -0400

    hw7

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
+// let board;
 let player1 = 'X';
 let player2 = 'O';
-
-const winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+let winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 let boxes = document.querySelectorAll(".box");
 start();
 
 function start(){
-    document.querySelector(".result").style.display="none";
+    document.querySelector(".finished").style.display="none";
     board = Array.from(Array(9).keys());
     for (var i = 0; i < boxes.length; i++){
         boxes[i].innerText = '';
         boxes[i].style.removeProperty('background-color');
-        boxes[i].addEventListener('click', filledSquare, false);
+        boxes[i].addEventListener('click', checkBox, false);
     }
 }
 
-function emptySquare(){
-    return board.filter(s => typeof s == 'number')
-}
-
-function filledSquare(box){
+function checkBox(box){
     if(typeof board[box.target.id] == 'number'){
         turn(box.target.id, player1);
-        if(!tie()) {turn(nextTurn(), player2);}
+        if(!tie()) {turn(nextSpot(), player2);}
     }
 }
 
@@ -34,14 +30,18 @@ function turn(boxId, player){
     if (gameWon) gameOver(gameWon)
 }
 
-function nextTurn(){
+function emptySquare(){
+    return board.filter(s => typeof s == 'number')
+}
+
+function nextSpot(){
     return emptySquare()[0];
 }
 
 function checkWin(board, player){
-    let plays = board.reduce((x,y,index) => (y === player) ? x.concat(index) : x, []);
+    let plays = board.reduce((x,y,index) => (y===player) ? 		x.concat(index) : x, []);
     let gameWon = null;
-    for (let [index, win] of winCombo.entries()){
+    for (let [index, win] of winningCombos.entries()){
         if (win.every(elem => plays.indexOf(elem)>-1)){
             gameWon = {index: index, player: player};
             break;
@@ -51,11 +51,12 @@ function checkWin(board, player){
 }
 
 function gameOver(gameWon){
-    for (let index of winCombo[gameWon.index]){
-        document.getElementById(index).style.backgroundColor = gameWon.player === player1 ? "green" : "blue";
+    for (let index of winningCombos[gameWon.index]){
+        document.getElementById(index).style.backgroundColor=
+            gameWon.player === player1 ? "green" : "green";
     }
     for(var i = 0; i < boxes.length; i++){
-        boxes[i].removeEventListener('click', filledSquare, false);
+        boxes[i].removeEventListener('click', checkBox, false);
     }
 }
 
@@ -63,7 +64,7 @@ function tie(){
     if (checkWin === null || emptySquare().length == 0){
         for(var i = 0; i < boxes.length; i++){
             boxes[i].style.backgroundColor = "red";
-            boxes[i].removeEventListener('click', filledSquare, false);
+            boxes[i].removeEventListener('click', checkBox, false);
         }
         return true;
     }
